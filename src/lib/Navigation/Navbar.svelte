7d75457883cc5b25a8872cdbb09495510841dfa2
Michael Reinegger <reinemic2.0@gmail.com>
2025-01-01 16:55:59 +0100

    refactor Navbar component to use separate global and section links; update props structure

--- src/lib/Navigation/Navbar.svelte
+++ src/lib/Navigation/Navbar.svelte
@@ -2,29 +2,50 @@
   import { Navbar, NavBrand, NavHamburger, NavUl, NavLi } from 'flowbite-svelte';
   import { DarkMode } from 'flowbite-svelte';
 
-  type NavbarProps = {
-    logos: {
-      light: string;
-      dark: string;
-    };
-    links: Array<{ href: string; text: string }>;
-  };
+  interface Props  {
+			logos: {
+				light: string;
+				dark: string;
+			};
+			globalLinks: Array<{ href: string; text: string }>;
+			sectionLinks: Array<{ href: string; text: string }>;
+		}
 
-  let props = $props<NavbarProps>();
+  const colors = [
+    'bg-red-200 hover:bg-red-300',
+    'bg-blue-200 hover:bg-blue-300',
+    'bg-green-200 hover:bg-green-300',
+    'bg-yellow-200 hover:bg-yellow-300',
+    'bg-purple-200 hover:bg-purple-300',
+    'bg-pink-200 hover:bg-pink-300'
+  ];
+
+  const getColor = (index: number) => colors[index % colors.length];
+
+  let { logos, globalLinks, sectionLinks } = $props();
 </script>
 
 <Navbar class="neo-brutalist-navbar border-4 border-black">
   <NavBrand href="/" class="neo-brutalist-brand">
-    <img src={props.logos.light} class="me-3 h-8 sm:h-10 dark:hidden" alt="MLJR Logo Light" />
-    <img src={props.logos.dark} class="me-3 h-8 sm:h-10 hidden dark:block" alt="MLJR Logo Dark" />
+    <img src={logos.light} class="me-3 h-8 sm:h-10 dark:hidden" alt="MLJR Logo Light" />
+    <img src={logos.dark} class="me-3 h-8 sm:h-10 hidden dark:block" alt="MLJR Logo Dark" />
     <span class="self-center whitespace-nowrap text-xl font-bold font-mono dark:text-white"></span>
   </NavBrand>
-  <NavUl class="h-full">
-    {#each props.links as link}
-      <NavLi href={link.href} class="neo-brutalist-nav-item mx-1">
-        {link.text}
-      </NavLi>
-    {/each}
+  <NavUl class="h-full flex gap-4">
+    <div class="flex items-center gap-2">
+      {#each sectionLinks as link, i}
+        <NavLi href={link.href} class="neo-brutalist-nav-item mx-1 {getColor(i)}">
+          {link.text}
+        </NavLi>
+      {/each}
+    </div>
+    <div class="flex items-center gap-2 border-l-2 border-gray-300 pl-4">
+      {#each globalLinks as link, i}
+        <NavLi href={link.href} class="neo-brutalist-nav-item mx-1 {getColor(i + sectionLinks.length)}">
+          {link.text}
+        </NavLi>
+      {/each}
+    </div>
   </NavUl>
   <div class="flex gap-4">
     <DarkMode class="neo-brutalist-button p-2 text-lg" />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,24 @@
-<script>
-	import Navbar from "$lib/Navigation/Navbar.svelte";
-	import Logo_h from "$lib/images/Logo_h.png";
-	import Logo_h_w from "$lib/images/Logo_h_w.png";
-	import Logo_h_b from "$lib/images/Logo_h_b.png";
+<script lang="ts">
+	import Navbar from '$lib/Navigation/Navbar.svelte';
+	import Logo_h from '$lib/images/Logo_h.png';
+	import Logo_h_w from '$lib/images/Logo_h_w.png';
+	import Logo_h_b from '$lib/images/Logo_h_b.png';
+
+	let navbarProps = {
+		logos: {
+			light: Logo_h_b,
+			dark: Logo_h_w
+		},
+		globalLinks: [
+			{ href: '/about', text: 'About' },
+			{ href: '/contact', text: 'Contact' }
+		],
+		sectionLinks: [
+			{ href: '/sudoku/easy', text: 'Easy Sudoku' },
+			{ href: '/sudoku/hard', text: 'Hard Sudoku' }
+		]
+	};
 
 </script>
-<Navbar
-logos={{
-	light: Logo_h_b,
-	dark: Logo_h_w
-}}
-links= {
-	[
-		{ href: "/", text: "Home" },
-		{ href: "/about", text: "About" },
-		{ href: "/contact", text: "Contact" }
-	]
-}
 
-/>
+<Navbar {...navbarProps}/>
