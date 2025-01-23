ae1cc16344908ed71e199862baa93c076561530d
Michael Reinegger <reinemic2.0@gmail.com>
2025-01-23 15:28:44 +0100

    add alias configuration in svelte.config.js; update tsconfig.json for module resolution; create utility functions for class name management

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
+        alias: {
+            $lib: 'src/lib',
+            $components: 'src/lib/components'
+        }
     },
     preprocess: vitePreprocess()
 };
